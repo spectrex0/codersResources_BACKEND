9c42be2b0b57f362fc4992f2f0d32cb73b56257c
spectre <alexandertejedadlr@gmail.com>
2025-05-05 08:48:47 -0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -26,7 +26,12 @@ const PORT = process.env.PORT || 3001;
 
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://codersresources.vercel.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(express.static(path.resolve(__dirname, "public")));
 
 
