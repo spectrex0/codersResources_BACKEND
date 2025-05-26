3390e1f50d30714c18dd996b6bef2a0abe4443ee
spectre <alexandertejedadlr@gmail.com>
2025-05-25 20:08:21 -0500

    delete feedback route added

--- src/routes/feedback/delete.ts
+++ src/routes/feedback/delete.ts
@@ -1,5 +1,21 @@
 import { Elysia, t } from "elysia";
+import feedbackModel from "../../models/feedbackModel.js";
 
-export const feedbackDeleteRoute = new Elysia().delete("/delete", ({}) => {}, {
-  body: t.Object({}),
+export const feedbackDeleteRoute = new Elysia().delete("/delete", async ({body}) => {
+  const {FeedbackContent} = body
+  try {
+    const deleteFeedback = await feedbackModel.findOneAndDelete({FeedbackContent})
+    return{
+      deleteFeedback,
+      message: "Feedback deleted ✔"
+    }
+  } catch (error) {
+    return{
+      message: "error deleting feedback"
+    }
+  }
+}, {
+  body: t.Object({
+    FeedbackContent: t.String(),
+  }),
 });
